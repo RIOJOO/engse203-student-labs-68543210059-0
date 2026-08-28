1c72eb58e29c4545f93991eaad03af1feb88f74d
RIOJOO <Surabordin2006@gmail.com>
2026-08-28 22:36:37 +0700

    fix(week-03): update asset paths to relative for hub deployment

--- labs/week-03/vite.config.js
+++ labs/week-03/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 
-const repositoryName = "engse203-lab02-68543210059-0";
-
 export default defineConfig({
-  base: `/${repositoryName}/labs/week-03/`,
+  base: "./",
   build: {
-    outDir: "docs",
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
