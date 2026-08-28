14b0c11f6ea2e5a9fde1627536bb44153a300a44
RIOJOO <Surabordin2006@gmail.com>
2026-08-28 22:13:13 +0700

    fix(week-02): fix vite base path to relative and update publish output

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 
-const repositoryName = "engse203-lab02-68543210059-0";
-
 export default defineConfig({
-  base: `/${repositoryName}/labs/week-02/`,
+  base: "./", 
   build: {
-    outDir: "docs",
+    outDir: "../publish", 
     emptyOutDir: true,
   },
 });
